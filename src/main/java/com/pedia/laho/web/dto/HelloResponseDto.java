980e31159b032ac33e711b1a0f0fa2390775a49e
Juyoung Bahng <qkdwn0313@gmail.com>
2022-01-16 20:13:01 +0900

    test : lombok Test

---
+++ src/main/java/com/pedia/laho/web/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package com.pedia.laho.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+}
---
+++ src/test/java/com/pedia/laho/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,26 @@
+package com.pedia.laho.web.dto;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복_기능_테스트() {
+        //given
+        String name = "lombok";
+        int amount = 12345;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
