96a2bb40bf5e0ab2dcf5bbe290f16f7eb3c819a9
Juyoung Bahng <qkdwn0313@gmail.com>
2022-01-22 21:58:37 +0900

    chore : modify repository

--- src/main/java/com/pedia/laho/domain/genre/GenreRepository.java
+++ src/main/java/com/pedia/laho/domain/genre/GenreRepository.java
@@ -2,5 +2,5 @@ package com.pedia.laho.domain.genre;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GenreRepository extends JpaRepository {
+public interface GenreRepository extends JpaRepository<Genre, Long> {
 }
--- src/main/java/com/pedia/laho/domain/junctionTable/MovieGenreRepository.java
+++ src/main/java/com/pedia/laho/domain/junctionTable/MovieGenreRepository.java
@@ -2,5 +2,5 @@ package com.pedia.laho.domain.junctionTable;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MovieGenreRepository extends JpaRepository {
+public interface MovieGenreRepository extends JpaRepository<MovieGenre, Long> {
 }
