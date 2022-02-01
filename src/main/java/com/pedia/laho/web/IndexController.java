42d317e8c9a37fb894d5068017ad7bb8dc645933
Juyoung Bahng <qkdwn0313@gmail.com>
2022-02-01 23:45:20 +0900

    feat : main page backend

--- src/main/java/com/pedia/laho/web/IndexController.java
+++ src/main/java/com/pedia/laho/web/IndexController.java
@@ -1,6 +1,7 @@
 package com.pedia.laho.web;
 
 import com.google.gson.*;
+import com.pedia.laho.web.dto.BoxOfficeResponseDto;
 import com.pedia.laho.web.dto.SearchResponseDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
@@ -11,10 +12,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 import java.util.logging.Logger;
 
 @CrossOrigin(origins = "http://localhost:3000")
@@ -26,9 +27,24 @@ public class IndexController {
     @Value("${kmdb.api.key}")
     private String kmdbKey;
 
+    @Value("${boxOffice.api.key}")
+    private String boxOfficeKey;
+
     @GetMapping("/")
-    public String index() {
-        return "Hello, LAHO-PEDIA";
+    @ResponseBody
+    public String index(HttpServletRequest request) throws IOException {
+        Calendar calendar = new GregorianCalendar();
+        SimpleDateFormat dayFormat = new SimpleDateFormat("yyyyMMdd");
+
+        calendar.add(Calendar.DATE, -1);
+        String day = dayFormat.format(calendar.getTime());
+
+        String urlBuilder = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?" +
+                "key=" + boxOfficeKey +
+                "&targetDt=" + day;
+
+        String response = apiRequest(urlBuilder);
+        return boxOfficeParsing(response);
     }
 
     @GetMapping("/api/search")
@@ -42,6 +58,61 @@ public class IndexController {
                 "&query=" + "\"" + keyWord + "\"" +
                 "&ServiceKey=" + kmdbKey;
 
+        String response = apiRequest(urlBuilder);
+        return searchParsing(response);
+    }
+
+    public String searchParsing(String jString) {
+        List<SearchResponseDto> result = new ArrayList<>();
+
+        JsonObject jObject = JsonParser.parseString(jString).getAsJsonObject();
+        JsonArray jArray = jObject.getAsJsonArray("Data");
+        JsonObject jArrayObject = jArray.get(0).getAsJsonObject();
+        JsonArray jArrayObjectArray = jArrayObject.getAsJsonArray("Result");
+
+        for (Object o : jArrayObjectArray) {
+            SearchResponseDto searchResponseDto;
+            JsonObject obj= (JsonObject) o;
+
+            String title = obj.get("title").getAsString();
+            String prodYear = obj.get("prodYear").getAsString();
+            String nation = obj.get("nation").getAsString();
+            String posters = obj.get("posters").getAsString();
+
+            searchResponseDto = new SearchResponseDto(title.trim(), prodYear.trim(), nation.trim(), posters.trim());
+            result.add(searchResponseDto);
+        }
+
+        String searchResult = new Gson().toJson(result);
+        logger.info(searchResult);
+        return searchResult;
+    }
+
+    public String boxOfficeParsing(String jString) {
+        List<BoxOfficeResponseDto> result = new ArrayList<>();
+
+        JsonObject jObject = JsonParser.parseString(jString).getAsJsonObject();
+        JsonObject jObjectObject= jObject.getAsJsonObject("boxOfficeResult");
+        JsonArray jArray = jObjectObject.getAsJsonArray("dailyBoxOfficeList");
+
+        for(Object o : jArray) {
+            BoxOfficeResponseDto boxOfficeResponseDto;
+            JsonObject obj = (JsonObject) o;
+
+            String rank = obj.get("rank").getAsString();
+            String title = obj.get("movieNm").getAsString();
+            String prodYear = obj.get("openDt").getAsString();
+
+            boxOfficeResponseDto = new BoxOfficeResponseDto(rank.trim(), title.trim(), prodYear.trim());
+            result.add(boxOfficeResponseDto);
+        }
+
+        String boxResult = new Gson().toJson(result);
+        logger.info(boxResult);
+        return boxResult;
+    }
+
+    public String apiRequest(String urlBuilder) throws IOException{
         URL url = new URL(urlBuilder);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
@@ -67,31 +138,6 @@ public class IndexController {
         rd.close();
         conn.disconnect();
 
-        return searchParsing(sb.toString());
-    }
-
-    public String searchParsing(String jString) {
-        List<SearchResponseDto> result = new ArrayList<>();
-
-        JsonObject jObject = JsonParser.parseString(jString).getAsJsonObject();
-        JsonArray jArray = jObject.getAsJsonArray("Data");
-        JsonObject jArrayObject = jArray.get(0).getAsJsonObject();
-        JsonArray jArrayObjectArray = jArrayObject.getAsJsonArray("Result");
-
-        for (Object o : jArrayObjectArray) {
-            SearchResponseDto searchResponseDto;
-            JsonObject obj= (JsonObject) o;
-            String title = obj.get("title").getAsString();
-            String prodYear = obj.get("prodYear").getAsString();
-            String nation = obj.get("nation").getAsString();
-            String posters = obj.get("posters").getAsString();
-
-            searchResponseDto = new SearchResponseDto(title.trim(), prodYear.trim(), nation.trim(), posters.trim());
-            result.add(searchResponseDto);
-        }
-
-        String searchResult = new Gson().toJson(result);
-        logger.info(searchResult);
-        return searchResult;
+        return sb.toString();
     }
 }
